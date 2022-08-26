3b206b499b3b120ccaabf821d5a2f38103d282e1
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-08-26 13:23:16 +0700

    Add files via upload

---
+++ kode2-24.cpp
@@ -0,0 +1,28 @@
+/**********************************************************
+*  PEMROGRAMAN C++: MUDAH & CEPAT MENJADI MASTER C++      *
+*                                                         *
+*  Oleh    : Budi Raharjo                                 *
+*  Email   : mbraharjo@gmail.com                          *
+*                                                         *
+*  Website : www.best-informatic.com                      *
+*                                                         *
+***********************************************************/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+  char S1[50] = "belajar";
+  char* S2 = (char *) "salin";
+
+  // Melakukan penyalinan string
+  // dari variabel S2 ke variabel S1
+  strncpy(S1, S2, 4); //dengan hanya menyalin 4 karakter pertama
+
+  // Menampilkan nilai dari variabel S1
+  cout<<S1<<endl;
+
+  return 0;
+}
